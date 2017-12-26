c4c3de40092ace76861842aa0d16a1630b25703d
太年轻 <tthd@163.com>
2017-12-26 08:59:24 +0800

    init

---
+++ src/Providers/UsersServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace One\Users\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class UsersServiceProvider extends ServiceProvider
+{
+	/**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+	 public function register()
+	 {
+
+	 }
+
+ 	/**
+	 * Register the application services.
+	 *
+	 * @return void
+	 */
+	 public function boot()
+	 {
+		 
+	 }
+}
